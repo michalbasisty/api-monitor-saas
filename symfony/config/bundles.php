164c39abce127b879da27228b855a0d908718e8a
michalbasisty <michalbasisty@github.com>
2025-11-12 13:06:21 +0100

    feat: Add Nelmio CORS configuration and remove conflicting security.yaml

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -6,6 +6,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 
     // Development bundles
